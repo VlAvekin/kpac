5993ba3ee2fc3bd5fd9e36a8b1d3bc4e784c7286
VlAvekin <https://github.com/VlAvekin>
2024-05-22 00:29:28 +0300

    init

---
+++ src/test/java/com/vladaviekin/kpac/KpacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladaviekin.kpac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
